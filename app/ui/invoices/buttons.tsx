3dbb0b9a78ba7c782a148bae2b7bf2381603b78d
satish singaram <satish.singaram@gmail.com>
2025-01-14 18:55:27 +0530

    fixed type error in button

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,6 +1,8 @@
+'use client';
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { deleteInvoice } from '@/app/lib/actions';
+import { deleteInvoice, State } from '@/app/lib/actions';
+import { useActionState } from "react";
 
 
 export function CreateInvoice() {
@@ -27,14 +29,17 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const initialState: State = { message: null, errors: {} };
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id); 
+  const [state, formAction] = useActionState(deleteInvoiceWithId, null);
+
   return (
-    <form action={deleteInvoiceWithId}>
+    <form action={formAction}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
     </form>
-
+ 
   );
 }
